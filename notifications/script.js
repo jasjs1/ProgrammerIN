f9ff1e0d6b3797b44909c7c6d97c0334d2fe929c
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-20 20:14:49 -0500

    i

---
+++ notifications/script.js
@@ -0,0 +1,20 @@
+const notification = document.getElementById('notification');
+const closeButton = document.querySelector('#notification .close');
+
+closeButton.addEventListener('click', () => {
+  notification.style.display = 'none';
+  console.log('Notification has been hidden');
+});
+
+const notificationMessages = [
+    "Hello there!",
+    "Welcome to our website!",
+    "Thanks for visiting!",
+    "Have a nice day!",
+    "You're awesome!",
+    "Keep up the good work!",
+  ];
+  
+  const randomIndex = Math.floor(Math.random() * notificationMessages.length);
+  const message = notificationMessages[randomIndex];
+  
