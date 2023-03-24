812b60a4eb433f9b41f4172e1dc7ceb41a56aa90
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-23 19:32:41 -0500

    description script added

--- account/profile/script.js
+++ account/profile/script.js
@@ -78,3 +78,114 @@ form.addEventListener('submit', event => {
 
 // Update the experiences list when the page loads
 updateExperiencesList();
+
+// Function to render list of basic skills
+// Function to render list of basic skills
+function renderSkills() {
+  const skillsList = document.getElementById('skills-list');
+  skillsList.innerHTML = '';
+  const skills = JSON.parse(localStorage.getItem('skills')) || [];
+  skills.forEach(skill => {
+    const skillItem = document.createElement('li');
+    skillItem.textContent = skill;
+    skillsList.appendChild(skillItem);
+  });
+}
+
+// Function to add a new skill
+function addSkill(skill) {
+  const skillsList = document.getElementById('skills-list');
+  const li = document.createElement('li');
+  li.textContent = skill;
+  skillsList.appendChild(li);
+}
+
+// Function to handle form submit
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const input = document.getElementById('new-skill');
+  const skill = input.value;
+  addSkill(skill);
+  input.value = '';
+
+  const skillsForm = document.querySelector('#skills-form');
+  const skillsList = document.querySelector('#skills-list');
+  let skillsArray = JSON.parse(localStorage.getItem('skills')) || [];
+  
+  // Function to render skills
+  function renderSkills() {
+    skillsList.innerHTML = '';
+    for (let i = 0; i < skillsArray.length; i++) {
+      const skillItem = document.createElement('li');
+      skillItem.textContent = skillsArray[i];
+      skillsList.appendChild(skillItem);
+    }
+  }
+  
+  // Function to handle form submission
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const skillInput = document.querySelector('#skill-input');
+    const skillValue = skillInput.value.trim();
+    if (skillValue) {
+      skillsArray.push(skillValue);
+      localStorage.setItem('skills', JSON.stringify(skillsArray));
+      skillInput.value = '';
+      renderSkills();
+    }
+  }
+  
+  // Attach event listeners
+  skillsForm.addEventListener('submit', handleFormSubmit);
+  
+  // Render initial skills
+  renderSkills();
+}
+
+
+const descriptionText = document.getElementById("description-text");
+const createDescriptionBtn = document.getElementById("create-description-btn");
+const descriptionModal = document.getElementById("description-modal");
+const saveDescriptionBtn = document.getElementById("save-description-btn");
+const cancelDescriptionBtn = document.getElementById("cancel-description-btn");
+const maxChars = 150;
+
+// Load description from local storage
+const description = localStorage.getItem("description");
+if (description) {
+  descriptionText.textContent = description;
+}
+
+// Open description modal
+createDescriptionBtn.addEventListener("click", () => {
+  descriptionModal.style.display = "block";
+  document.getElementById("new-description").value = description || "";
+});
+
+
+saveDescriptionBtn.addEventListener("click", () => {
+    const newDescription = document.getElementById("new-description").value;
+    const charCount = newDescription.length;
+    
+    if (charCount > 150) {
+      alert(`Description exceeds maximum length by ${charCount - 150} characters. The description character length needs to be 150 characters`);
+    } else {
+      localStorage.setItem("description", newDescription);
+      descriptionText.textContent = newDescription;
+      descriptionModal.style.display = "none";
+    }
+  });
+  
+  
+
+// Close description modal
+cancelDescriptionBtn.addEventListener("click", () => {
+  descriptionModal.style.display = "none";
+  document.textContent('No description');
+});
+
+// Edit description
+descriptionText.addEventListener("click", () => {
+  descriptionModal.style.display = "block";
+  document.getElementById("new-description").value = descriptionText.textContent || "";
+});
